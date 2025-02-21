e20df847d001c2355dfee6948ff19c2989aa2452
kota113 <kota113@keio.jp>
2025-02-21 09:30:46 +0900

    add a bit of feature

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,6 +1,7 @@
 package com.example.demo
 
 import com.example.demo.entity.SettingsDatum
+import com.example.demo.entity.WordCount
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.repository.CrudRepository
@@ -22,10 +23,40 @@ class GuildSettingsResource(val service: GuildSettingsService) {
 }
 
 @Service
-class GuildSettingsService(val db: GuildRepository) {
+class GuildSettingsService(val db: GuildSettingsRepository) {
     fun get(guildId: Long) = db.findById(guildId)
 }
 
-interface GuildRepository: CrudRepository<SettingsDatum, Long> {
+interface GuildSettingsRepository: CrudRepository<SettingsDatum, Long> {}
 
+
+@RestController
+@RequestMapping("/guilds/{guildId}/connection_state")
+class ConnectionStateResource(val service: ConnectionStateService) {
+    @PostMapping
+    fun post(@RequestBody props: ConnectionStateProps, @PathVariable guildId: Long) {
+        service.create(guildId, props)
+    }
 }
+
+@Service
+class ConnectionStateService(val settingsDb: GuildSettingsRepository, val characterUsageDb: CharacterUsageRepository) {
+    fun create(guildId: Long, props: ConnectionStateProps) {
+        val settings = settingsDb.findById(guildId)
+        val characterUsages = characterUsageDb.findById(guildId)
+        TODO()
+    }
+}
+
+interface CharacterUsageRepository: CrudRepository<WordCount, Long> {}
+
+data class ConnectionStateProps(
+    val vcId: Long,
+    val tcId: Long,
+    val readGuild: Boolean?,
+    val readName: Boolean?,
+    val speechSpeed: Float?,
+    val lang: String?,
+    val maxMessageLength: Int?,
+    val translate: Boolean?
+)
